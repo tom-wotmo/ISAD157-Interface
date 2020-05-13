bdd6f7e9bfb082d831d2027b93ae448827f9cb1e
tom-wotmo <geobite@hotmail.co.uk>
2020-05-13 11:20:25 +0100

    Initial commit

---
+++ DatabaseForm.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace SOFT562Week18
+{
+    partial class DatabaseForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewCustomer = new System.Windows.Forms.DataGridView();
+            this.dataGridViewProduct = new System.Windows.Forms.DataGridView();
+            this.comboBoxQueryChoice = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCustomer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewProduct)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewCustomer
+            // 
+            this.dataGridViewCustomer.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewCustomer.Location = new System.Drawing.Point(20, 15);
+            this.dataGridViewCustomer.Name = "dataGridViewCustomer";
+            this.dataGridViewCustomer.RowHeadersWidth = 51;
+            this.dataGridViewCustomer.RowTemplate.Height = 24;
+            this.dataGridViewCustomer.Size = new System.Drawing.Size(368, 370);
+            this.dataGridViewCustomer.TabIndex = 0;
+            // 
+            // dataGridViewProduct
+            // 
+            this.dataGridViewProduct.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewProduct.Location = new System.Drawing.Point(414, 15);
+            this.dataGridViewProduct.Name = "dataGridViewProduct";
+            this.dataGridViewProduct.RowHeadersWidth = 51;
+            this.dataGridViewProduct.RowTemplate.Height = 24;
+            this.dataGridViewProduct.Size = new System.Drawing.Size(364, 370);
+            this.dataGridViewProduct.TabIndex = 1;
+            // 
+            // comboBoxQueryChoice
+            // 
+            this.comboBoxQueryChoice.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxQueryChoice.FormattingEnabled = true;
+            this.comboBoxQueryChoice.Items.AddRange(new object[] {
+            "Customers Table",
+            "Products Table"});
+            this.comboBoxQueryChoice.Location = new System.Drawing.Point(20, 404);
+            this.comboBoxQueryChoice.Name = "comboBoxQueryChoice";
+            this.comboBoxQueryChoice.Size = new System.Drawing.Size(757, 34);
+            this.comboBoxQueryChoice.TabIndex = 2;
+            this.comboBoxQueryChoice.SelectedIndexChanged += new System.EventHandler(this.comboBoxQueryChoice_SelectedIndexChanged);
+            // 
+            // DatabaseForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBoxQueryChoice);
+            this.Controls.Add(this.dataGridViewProduct);
+            this.Controls.Add(this.dataGridViewCustomer);
+            this.Name = "DatabaseForm";
+            this.Text = "Database Form Test";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCustomer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewProduct)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewCustomer;
+        private System.Windows.Forms.DataGridView dataGridViewProduct;
+        private System.Windows.Forms.ComboBox comboBoxQueryChoice;
+    }
+}
+
---
+++ DatabaseForm.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace SOFT562Week18
+{
+    public partial class DatabaseForm : Form
+    {
+        public DatabaseForm()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBoxQueryChoice_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBoxQueryChoice.SelectedIndex == 0)
+            {
+                /////////////////////////////////////////////////////////////////
+                //
+                // The basic format of a connection string consists of a series
+                // of keyword/value pairs separated by semicolons. The equal
+                // sign (=) connects each keyword and its value. Specifically,
+                // our connection string MUST refer to 
+                // proj-mysql.uopnet.plymouth.ac.uk as the server, and
+                // to our username and password to access
+                // proj-mysql.uopnet.plymouth.ac.uk.
+                //
+                /////////////////////////////////////////////////////////////////
+
+                string connectionString = "SERVER=" + DBConnect.SERVER + ";" +
+                    "DATABASE=" + DBConnect.DATABASE_NAME + ";" + "UID=" +
+                    DBConnect.USER_NAME + ";" + "PASSWORD=" +
+                    DBConnect.PASSWORD + ";" + "SslMode=" +
+                    DBConnect.SslMode + ";";
+
+                ///////////////////////////////////////////////////////////////
+                //
+                // Create an open connection to a MySQL Server database. In our
+                // case, the connection allows us to interact with
+                // proj-mysql.uopnet.plymouth.ac.uk.
+                //
+                ///////////////////////////////////////////////////////////////
+
+                using (MySqlConnection connection =
+                    new MySqlConnection(connectionString))
+                {
+                    ///////////////////////////////////////////////////////////
+                    //
+                    // The query which retrieves all the records in the
+                    // customers table.
+                    //
+                    ///////////////////////////////////////////////////////////
+                    string query = "SELECT * FROM soft562_marco.customer";
+
+                    ///////////////////////////////////////////////////////////
+                    //
+                    // Opens a database connection with the property settings
+                    // specified by the ConnectionString.
+                    //
+                    ///////////////////////////////////////////////////////////
+                    connection.Open();
+
+                    ///////////////////////////////////////////////////////////
+                    //
+                    // Submit the SQL statement to be executed against the
+                    // MySQL database.
+                    //
+                    ///////////////////////////////////////////////////////////
+                    MySqlCommand cmd = new MySqlCommand(query, connection);
+
+                    ///////////////////////////////////////////////////////////
+                    //
+                    // The MySqlDataAdapter represents a set of data commands
+                    // and a database connection that are used to fill a
+                    // dataset and update a MySQL database.
+                    //
+                    ///////////////////////////////////////////////////////////
+                    MySqlDataAdapter sqlDA = new MySqlDataAdapter(cmd);
+                    DataTable customerDataTable = new DataTable();
+                    sqlDA.Fill(customerDataTable);
+
+                    ///////////////////////////////////////////////////////////
+                    //
+                    // Bind the ACME customer table to the Data Grid View.
+                    //
+                    ///////////////////////////////////////////////////////////
+                    dataGridViewCustomer.DataSource = customerDataTable;
+
+                } // End of using (MySqlConnection connection = ...
+            }
+            else
+            {
+                /////////////////////////////////////////////////////////////////
+                //
+                // The basic format of a connection string consists of a series
+                // of keyword/value pairs separated by semicolons. The equal
+                // sign (=) connects each keyword and its value. Specifically,
+                // our connection string MUST refer to 
+                // proj-mysql.uopnet.plymouth.ac.uk as the server, and
+                // to our username and password to access
+                // proj-mysql.uopnet.plymouth.ac.uk.
+                //
+                /////////////////////////////////////////////////////////////////
+
+                string connectionString = "SERVER=" + DBConnect.SERVER + ";" +
+                    "DATABASE=" + DBConnect.DATABASE_NAME + ";" + "UID=" +
+                    DBConnect.USER_NAME + ";" + "PASSWORD=" +
+                    DBConnect.PASSWORD + ";" + "SslMode=" +
+                    DBConnect.SslMode + ";";
+
+                ///////////////////////////////////////////////////////////////
+                //
+                // Create an open connection to a MySQL Server database. In our
+                // case, the connection allows us to interact with
+                // proj-mysql.uopnet.plymouth.ac.uk.
+                //
+                ///////////////////////////////////////////////////////////////
+
+                using (MySqlConnection connection =
+                    new MySqlConnection(connectionString))
+                {
+                    ///////////////////////////////////////////////////////////
+                    //
+                    // The query which retrieves all the records in the
+                    // customers table.
+                    //
+                    ///////////////////////////////////////////////////////////
+                    string query = "SELECT * FROM soft562_marco.product";
+
+                    ///////////////////////////////////////////////////////////
+                    //
+                    // Opens a database connection with the property settings
+                    // specified by the ConnectionString.
+                    //
+                    ///////////////////////////////////////////////////////////
+                    connection.Open();
+
+                    ///////////////////////////////////////////////////////////
+                    //
+                    // Submit the SQL statement to be executed against the
+                    // MySQL database.
+                    //
+                    ///////////////////////////////////////////////////////////
+                    MySqlCommand cmd = new MySqlCommand(query, connection);
+
+                    ///////////////////////////////////////////////////////////
+                    //
+                    // The MySqlDataAdapter represents a set of data commands
+                    // and a database connection that are used to fill a
+                    // dataset and update a MySQL database.
+                    //
+                    ///////////////////////////////////////////////////////////
+                    MySqlDataAdapter sqlDA = new MySqlDataAdapter(cmd);
+                    DataTable productDataTable = new DataTable();
+                    sqlDA.Fill(productDataTable);
+
+                    ///////////////////////////////////////////////////////////
+                    //
+                    // Bind the ACME customer table to the Data Grid View.
+                    //
+                    ///////////////////////////////////////////////////////////
+                    dataGridViewProduct.DataSource = productDataTable;
+                }
+            } // End of else...
+        } // End of private void comboBoxQueryChoice_SelectedIndexChanged
+    }
+}
